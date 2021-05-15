52cea078ef50d7c78796bcf5c327ecad95a9405f
Leo <efrenn98@gmail.com>
2021-05-15 04:24:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(array) {
+    let year = array.find(res => res.result === 'W')
+     if (year) {
+         return year.year
+     }else{
+         return undefined
+     }
+ 
+ }
